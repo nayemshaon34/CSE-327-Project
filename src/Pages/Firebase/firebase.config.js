2d2d3985e0b5e1856a1995dc3a91f137eac8b016
nayemshaon34 <nayemshaon34@gmail.com>
2022-04-16 14:10:21 +0600

    firebase setup done

---
+++ src/Pages/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHae2279CApqr_luYd2wI5tpNX8FYg4DE",
+    authDomain: "tour-faster.firebaseapp.com",
+    projectId: "tour-faster",
+    storageBucket: "tour-faster.appspot.com",
+    messagingSenderId: "219073565559",
+    appId: "1:219073565559:web:89c4ff206dc452e601c772",
+    measurementId: "G-RV3P0CBE2Z"
+  };
+
+  export default firebaseConfig;
