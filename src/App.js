4da7d805842e1c4512b747009fc3de30bc33afc2
nayemshaon34 <nayemshaon34@gmail.com>
2022-04-16 22:47:24 +0600

    home init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h3>This is a webpage</h3>
+      <Home></Home>
     </div>
   );
 }
