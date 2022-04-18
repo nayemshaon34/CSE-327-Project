4ca503d2b5723c010d8e5a30817101c85ccdefe0
nayemshaon34 <nayemshaon34@gmail.com>
2022-04-19 04:35:46 +0600

    Login Reg Added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import AuthProvider from './Context/AuthProvider';
-import TopHeader from './Pages/Shared/Header/TopHeader/TopHeader';
-import Header from './Pages/Shared/Header/Header/Header';
-import Footer from './Pages/Shared/Footer/Footer/Footer';
-
-
-function App() {
-  return (
-=======
-<<<<<<< Updated upstream
-
-function App() {
-  return (
-    <div className="App">
-      <h3>This is a webpage</h3>
-=======
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AuthProvider from './Context/AuthProvider';
 import TopHeader from './Pages/Shared/Header/TopHeader/TopHeader';
@@ -28,15 +10,12 @@ import Register from './Pages/Register/Register';
 
 function App() {
   return (
->>>>>>> login
     <div>
       <AuthProvider>
         <Router>
           <TopHeader></TopHeader>
          <Header></Header>
-<<<<<<< HEAD
-=======
-          <Switch>
+         <Switch>
           <Route path="/login">
               <Login></Login>
             </Route>
@@ -44,15 +23,9 @@ function App() {
               <Register></Register>
             </Route>
           </Switch>
->>>>>>> login
-         
           <Footer></Footer>
         </Router>
       </AuthProvider>
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
->>>>>>> login
     </div>
   );
 }
