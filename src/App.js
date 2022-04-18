4a0534436900039fc033e40cc270c36d7dd07791
nayemshaon34 <nayemshaon34@gmail.com>
2022-04-19 04:27:28 +0600

    Login Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+<<<<<<< Updated upstream
 
 function App() {
   return (
     <div className="App">
       <h3>This is a webpage</h3>
+=======
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AuthProvider from './Context/AuthProvider';
+import TopHeader from './Pages/Shared/Header/TopHeader/TopHeader';
+import Header from './Pages/Shared/Header/Header/Header';
+import Footer from './Pages/Shared/Footer/Footer/Footer';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
+
+
+function App() {
+  return (
+    <div>
+      <AuthProvider>
+        <Router>
+          <TopHeader></TopHeader>
+         <Header></Header>
+          <Switch>
+          <Route path="/login">
+              <Login></Login>
+            </Route>
+            <Route path="/register">
+              <Register></Register>
+            </Route>
+          </Switch>
+         
+          <Footer></Footer>
+        </Router>
+      </AuthProvider>
+>>>>>>> Stashed changes
     </div>
   );
 }
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,94 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form"
+import { useHistory, useLocation } from 'react-router';
+import './Login.css';
+import logo from '../../Images/logo-dark.png';
+import { Link } from 'react-router-dom';
+import useAuth from '../../Hooks/useAuth';
 const Login = () => {
-    return (
-        <div>
-            
+  const { singInUsingGoogle, user, singInwithEpass, error } = useAuth();
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = data => {
+    singInwithEpass(data.email, data.password)
+      .then(result => {
+
+        if (error) {
+          history.push(redirect_uri);
+        }
+      })
+  };
+
+  const location = useLocation();
+  const history = useHistory();
+  const redirect_uri = location.state?.from || '/home'
+  const handelGooglesingin = () => {
+    singInUsingGoogle()
+      .then(result => {
+        history.push(redirect_uri);
+      })
+  }
+
+
+
+  return (
+    <div className="a">
+
+      {/* handle error */}
+
+
+      {
+        error && <div className="errors d-flex justify-content-center mt-1 bg-danger">
+          <span className="fs-4 text-light">{error.slice(5)}</span>
         </div>
-    );
+      }
+      <div className="container">
+
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-4">
+            <div className="shadow my-5 p-4 rounded">
+              <div className="dark-logo pt-3 d-flex justify-content-center">
+                <img src={logo} alt="" className="" />
+              </div>
+              <div className="">
+
+                {/* login form  */}
+
+                <form className="form-design" onSubmit={handleSubmit(onSubmit)}>
+                  <div className="text-input">
+                    <input type="email" placeholder="Enter your email" id="email" {...register("email", { required: true })} />
+                    <label htmlFor="email">Email</label>
+                  </div>
+
+                  <div className="text-input">
+                    <input type="password" id="password" placeholder="Enter your password"{...register("password", { required: true, min: 18, max: 99 })} />
+                    <label htmlFor="password">Password</label>
+                  </div>
+
+                  <div className="d-flex justify-content-center my-4">
+                    <input type="submit" value="Log In" />
+                  </div>
+                  <div className="a">
+                    <div className="text-center mt-4">
+                      <p>-------------- OR --------------</p>
+                    </div>
+
+                    <div className="text-center loginwth-g border">
+                      <button onClick={handelGooglesingin} className="btn fs-5 fw-bold"><i className="fab fa-google text-warning"></i> Log in with Google</button>
+                    </div>
+                    <div className="footer border my-4 mt-5">
+                      <div className="p-2 text-center">
+                        <span className="a">Don't have an account? <Link to="/register">Register</Link></span>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
